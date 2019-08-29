a3961d734288ae41cc44ae017f2edf8764f54dcb
insideoutzombie <david@web3devs.com>
2019-08-29 11:39:35 -0500

    update to code add array plus add function

--- bruno.py
+++ bruno.py
@@ -1,36 +1,4 @@
-# Create a task list. A user is presented with the text below.
-# Let them select an option to list all of their tasks, add a task to their list, delete a task, or quit the program.
-# Make each option a different function in your program.
-# Do NOT use Google. Do NOT use other students. Try to do this on your own.
-
-# Congratulations! You're running [YOUR NAME]'s Task List program.
-
-# What would you like to do next?
-# 1. List all tasks.
-# 2. Add a task to the list.
-# 3. Delete a task.
-# 0. To quit the program
-
-# class Task:
-#     def __init__(self, task):
-
-userName = input("Enter your name: ")
-print(f"Congratulations! You are running {userName}'s Task List program.")
-
-class Task:
-    def __init__(self, task):
-        self.task = task
-
-    def add_task(self, newTask):
-        self.task = self.task + ", " + newTask
-        return newTask
-
-    def allTasks(self):
-        print(f"{self.task}")
-
-newTask1 = Task("1. Wash Car")
-newTask2 = Task("2. Clean Floors")
-newTask3 = Task("3. Walk Dogs")
+task_array = ["Clean house", "wash dog", "clean car"]
 
 def main():
     userInput = print("What would you like to do next? ")
@@ -41,11 +9,12 @@ def main():
                               "3. Delete a task.\n"
                               "0. To quit the program\n"))
         if userInput == 1:
-            taskInput = input("Enter a new task ")
-            newTask1.add_task(taskInput)
-            newTask1.allTasks()
+            print("blank")
         elif userInput == 2:
-            print("Add a task to the list")
+            taskInput = input("Enter a new task ")
+            # taskInput = str(task_array) + taskInput
+            print(str(task_array) + str(taskInput))
+            print("Success task added ")
         elif userInput == 3:
-            print("Delete a task")
+            print("DeleteTask")
 main()
